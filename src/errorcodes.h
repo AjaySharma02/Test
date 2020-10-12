37151f57cb21d4a865cd094c2470f88482848d9f
Jenkins <jenkins@ip-172-31-0-254.us-west-2.compute.internal>
2020-10-12 08:43:31 -0500

    update repo Two

---
+++ src/errorcodes.h
@@ -0,0 +1,21 @@
+//
+//  errorcodes.h
+//  VIZIO Second Screen Protocol Error Codes
+//
+//  Created by Bill Baxter on 3/30/15.
+//  Copyright (c) 2015 Bill Baxter. All rights reserved.
+//
+
+#define API_ERR_FAILURE -1
+#define API_ERR_SUCCESS 0
+#define API_ERR_BLOCKED 1
+#define API_ERR_BUSY 2
+#define API_ERR_ABORTED 3
+#define API_ERR_INVALID_PARAMETER 4
+#define API_ERR_MISSING_PARAMETER 5
+#define API_ERR_VALUE_OUT_OF_RANGE 6
+#define API_ERR_TYPE_ERROR 7
+#define API_ERR_HASHVAL_ERROR 8
+#define API_ERR_URI_NOT_FOUND 9
+#define API_ERR_READ_ONLY_ERROR 10
+
